51cc51d7cadb314b4f88fa84358eada1ded18147
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-03-19 12:00:19 +0100

    Ajout de l'authentification basique dans le middleware secureOrigin

--- middlewares/secureOrigin.js
+++ middlewares/secureOrigin.js
@@ -1,8 +1,23 @@
-const { USER_AGENT_ENV } = process.env;
+const { USER_AGENT_ENV, BACKEND_USERNAME, BACKEND_PASSWORD } = process.env;
 
 const secureOrigin = (req, res, next) => {
 	const allowedOrigins = ["http://localhost:4001", "https://ipseis-git-test-joachim-jasmins-projects.vercel.app", "https://www.ipseis.fr"];
 	if (USER_AGENT_ENV === "dev" || req.url === "/" || req.url.startsWith("/stylesheets")) {
+		return next();
+	}
+
+	// Vérification de l'authentification basique
+	const authHeader = req.headers["authorization"];
+	if (!authHeader) {
+		return res.status(401).send("Authentification requise");
+	}
+
+	const base64Credentials = authHeader.split(" ")[1];
+	const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
+	const [username, password] = credentials.split(":");
+
+	// Remplacez ces valeurs par vos identifiants sécurisés
+	if (username === BACKEND_USERNAME && password === BACKEND_PASSWORD) {
 		return next();
 	}
 
@@ -32,6 +47,8 @@ const secureOrigin = (req, res, next) => {
 	if (isUserAgentBlocked && isOriginBlocked) {
 		return res.status(403).send("Accès refusé");
 	}
+
+	next();
 };
 
 module.exports = secureOrigin;
