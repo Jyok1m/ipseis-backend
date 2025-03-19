605331aeb031cad212da5c40244ff2f5748b579c
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-03-19 11:00:40 +0100

    Suppression de la route des utilisateurs et nettoyage du fichier app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 var cors = require("cors");
@@ -18,6 +17,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
