fc5ff159dc0cb3a732f30e8917b8f7426dfd1f37
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-03-19 11:26:01 +0100

    Ajout d'une route pour récupérer la liste des thèmes avec gestion des erreurs

--- routes/themes.js
+++ routes/themes.js
@@ -2,4 +2,14 @@ var express = require("express");
 var router = express.Router();
 var db = require("../db/db");
 
+router.get("/list", async function (req, res) {
+	try {
+		const themes = await db.themes.find().lean().select("-trainings");
+		res.json(themes);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Erreur interne. Merci de réessayer plus tard." });
+	}
+});
+
 module.exports = router;
